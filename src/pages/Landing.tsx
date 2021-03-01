a938e020e1199fbf7d84178cc16f18728d181f38
luis <luis.sg.vitor@gmail.com>
2021-03-01 17:57:49 -0300

    Refactoring Landing Page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,63 +3,60 @@ import { Link } from 'react-router-dom'
 
 import '../styles/pages/landing.css'
 
-//const logo_cadern = require('../images/logo-caderno.svg')
-
-//const logoCaderno = require('../images/logo-caderno.svg')
-
-import logo_caderno  from '../images/logo-caderno.svg' 
-import logo_productive from '../images/logo-productive.svg'
-import logo_seguranca from '../images/logo-seguranca.svg'
 import logo from '../images/logo.svg'
+import logoNotebook from '../images/logo-caderno.svg'
+import logoSecurity from '../images/logo-seguranca.svg'
+import logoProductive from '../images/logo-productive.svg'
 
 function Landing() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <div className="left-content">
-          <div className="info">
-            <div className="description">
-              <img src={logo_caderno} alt=""/>
+    <div className="landing-page">
+      <div className="left-content">
+        <div className="l-content-wrapper">
+          <div className="line-note">
+            <img src={logoNotebook} alt="notebook" />
+            <div className="detail">
               <span>Save your notes</span>
             </div>
-
-            <div className="description">
-              <img src={logo_seguranca} alt="logo_seguranca" />
-              <span>Keep safe</span>
-            </div>
-
-            <div className="description">
-            <img src={logo_productive} alt="logo_productive" />
-            <span>Be productive</span>
+          </div>
+          <div className="line-note">
+            <img src={logoSecurity} alt="security" />
+            <div className="detail">
+              <span>Keep safe </span>
             </div>
           </div>
-        </div>
-        <div className="right-content">
-          <div className="info">
-            <img src={logo} alt="logo_app" />
-
-            <div className="main">
-              <span>When you plan what you need to do you are being best.</span>
-            </div>
-            <div className="secundary">
-              <span>Join app now</span>
-            </div>
-
-            <div className="buttons">
-              <Link to="/register" className="button-link">
-                <div className="button-sign-up">
-                  <span>Sign up</span>
-                </div>
-              </Link>
-              <Link to="/login" className="button-link">
-                <div className="button-login">
-                  <span>Log in</span>
-                </div>
-              </Link>
+          <div className="line-note-last">
+            <img src={logoProductive} alt="productive" />
+            <div className="detail">
+              <span>Be productive</span>
             </div>
           </div>
         </div>
       </div>
+      <div className="right-content">
+        <div className="r-content-wrapper">
+          <img src={logo} alt="logo" />
+          <div className="description-main">
+            <span>When you plan what you need to do you are being best.</span>
+          </div>
+          <div className="description-sub">
+            <span>Join app now</span>
+          </div>
+          <div className="buttons">
+            <Link to="/register" className="sign-up" href="#">
+              <div className="text-button">
+                <span>Sign Up</span>
+              </div>
+            </Link>
+            <Link to="login" className="login" href="#">
+              <div className="text-button">
+                <span>Log In</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 };
