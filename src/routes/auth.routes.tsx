b18f4fc63ab423404587743273187abdf07ec747
luis <luis.sg.vitor@gmail.com>
2021-03-05 18:54:51 -0300

    Refactoring routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Landing from './pages/Landing'
-import Home from './pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Landing from '../pages/Landing'
 
-function Routes() {
+function AuthRoutes(){
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
-        <Route path="/home" component={Home} />
       </Switch>
     </BrowserRouter>
   )
 }
 
-export default Routes;
+export default AuthRoutes;
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import AuthRoutes from './auth.routes'
+
+function Routes (){
+  return (
+    <AuthRoutes />
+  )
+}
+
+export default Routes;
