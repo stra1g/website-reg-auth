1e5e42a0a48f40148727560845d28f28692eb075
luis <luis.sg.vitor@gmail.com>
2021-03-05 19:37:58 -0300

    Creating Auth Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './styles/global.css'
 
+import AuthContext from './contexts/auth'
+
 import Routes from './routes/'
 
 function App() {
   return (
-    <Routes />
+    <AuthContext.Provider value={{signed: true}}>
+      <Routes />
+    </AuthContext.Provider>
   );
 }
 
