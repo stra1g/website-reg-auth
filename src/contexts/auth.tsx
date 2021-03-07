1c8b62166a3b54734ae9f098e457f07b504fab76
luis <luis.sg.vitor@gmail.com>
2021-03-06 22:26:43 -0300

    adding redirect system

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,24 +1,30 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import api from '../services/api'
+
+import history from '../utils/history'
 
 interface AuthContextData{
   signed: boolean;
-  user: object;
+  user: object | null;
   signIn(data:object): Promise<void>
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
-
 const AuthProvider: React.FC = ({children}) => {
+  
+  const [user, setUser] = useState<object | null>(null)
+
   async function signIn(data:object){
+    
     const response = await api.post('login', data)
-    const { token, user } = response.data
-    console.log(response.data)
+
+    history.push('/home')
+    setUser(response.data.filteredUser)
   }
 
   return (
-    <AuthContext.Provider value={{signed: false, user: {}, signIn }}>
+    <AuthContext.Provider value={{signed: Boolean(user), user, signIn }}>
       {children}
     </AuthContext.Provider>
   )
