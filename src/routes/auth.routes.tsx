ac121243d18a7d9dfee557f69dbe05220c92dff9
luis <luis.sg.vitor@gmail.com>
2021-03-25 17:52:08 -0300

    Creating route to PasswordUpdate

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -5,6 +5,7 @@ import Login from '../pages/Login'
 import Register from '../pages/Register'
 import Landing from '../pages/Landing'
 import PasswordReset from '../pages/PasswordReset'
+import PasswordUpdate from '../pages/PasswordUpdate'
 
 function AuthRoutes(){
   return (
@@ -13,7 +14,8 @@ function AuthRoutes(){
         <Route path="/" exact component={Landing} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
-        <Route path="/password-reset" component={PasswordReset} />
+        <Route path="/password-sendLink" component={PasswordReset} />
+        <Route path="/password-reset" component={PasswordUpdate} />
       </Switch>
     </BrowserRouter>
   )
